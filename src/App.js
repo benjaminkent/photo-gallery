c2216ac2d7f1afbd272caaf06c8cbd3786bbf5ab
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-10-03 14:12:26 -0400

    creating content from imported json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
+import Json from './Json.json'
 
 class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      pandas: {
-        title: 'Panda Bears',
-        description: 'Pandas are bears in China and are super cute',
-        imageURL:
-          'https://cdn-images-1.medium.com/max/1600/1*i1vVm3EqqDIkyucD0079wg.jpeg'
-      },
-      miniatures: {
-        title: 'Miniature Painting',
-        description: 'I enjoy miniature painting',
-        imageURL:
-          'https://instagram.ftpf1-1.fna.fbcdn.net/vp/ac13de8676b9e7d7b3a4f1aaf38a9a55/5C3728A2/t51.2885-15/e35/32203464_178578342778306_8009127367152762880_n.jpg'
-      }
-    }
+    this.state = Json
   }
   render() {
     return (
@@ -31,12 +19,12 @@ class App extends Component {
           <article className="category">
             <h3>{this.state.pandas.title}</h3>
             <p>{this.state.pandas.description}</p>
-            <img src={this.state.pandas.imageURL} />
+            <img src={this.state.pandas.photos[0].imageURL} />
           </article>
           <article className="category">
             <h3>{this.state.miniatures.title}</h3>
             <p>{this.state.miniatures.description}</p>
-            <img src={this.state.miniatures.imageURL} />
+            <img src={this.state.miniatures.photos[0].imageURL} />
           </article>
         </main>
       </div>
