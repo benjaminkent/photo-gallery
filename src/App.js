92ec7c0f6e844b9f520a9fb83f557b1fc434a190
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-10-03 14:04:27 -0400

    created page from state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pandas: {
+        title: 'Panda Bears',
+        description: 'Pandas are bears in China and are super cute',
+        imageURL:
+          'https://cdn-images-1.medium.com/max/1600/1*i1vVm3EqqDIkyucD0079wg.jpeg'
+      },
+      miniatures: {
+        title: 'Miniature Painting',
+        description: 'I enjoy miniature painting',
+        imageURL:
+          'https://instagram.ftpf1-1.fna.fbcdn.net/vp/ac13de8676b9e7d7b3a4f1aaf38a9a55/5C3728A2/t51.2885-15/e35/32203464_178578342778306_8009127367152762880_n.jpg'
+      }
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -11,20 +29,14 @@ class App extends Component {
         </header>
         <main>
           <article className="category">
-            <h3>Panda Bears</h3>
-            <p>
-              Pandas are bears native to south-central China, and are
-              objectively the cutest animals on Earth.
-            </p>
-            <img src="https://cdn-images-1.medium.com/max/1600/1*i1vVm3EqqDIkyucD0079wg.jpeg" />
+            <h3>{this.state.pandas.title}</h3>
+            <p>{this.state.pandas.description}</p>
+            <img src={this.state.pandas.imageURL} />
           </article>
           <article className="category">
-            <h3>Miniature Painting</h3>
-            <p>
-              I enjoy painting miniatures. I've only been painting for about
-              6-months , here's some of my work
-            </p>
-            <img src="https://instagram.ftpf1-1.fna.fbcdn.net/vp/ac13de8676b9e7d7b3a4f1aaf38a9a55/5C3728A2/t51.2885-15/e35/32203464_178578342778306_8009127367152762880_n.jpg" />
+            <h3>{this.state.miniatures.title}</h3>
+            <p>{this.state.miniatures.description}</p>
+            <img src={this.state.miniatures.imageURL} />
           </article>
         </main>
       </div>
