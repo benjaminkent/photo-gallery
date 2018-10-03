bff2968f52e4a6260913070d0b9106069fac6e7b
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-03 19:10:55 -0400

    photo list started

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react'
 import './App.css'
 import CategoryList from './CategoryList'
 
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <h1>Things I Like</h1>
-          <h2>A Photo Gallery by Benjamin Kent Jehl</h2>
-        </header>
-        <CategoryList />
+        <Router>
+          <div>
+            <header>
+              <h1>Things I Like</h1>
+              <h2>A Photo Gallery by Benjamin Kent Jehl</h2>
+            </header>
+            <CategoryList />
+            <div>
+              <ul className="photo-list-header">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>/</li>
+                <li>Panda Bears</li>
+              </ul>
+            </div>
+          </div>
+        </Router>
       </div>
     )
   }
