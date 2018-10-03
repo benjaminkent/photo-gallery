908a50b64f743871a3fff3741d0937ba914c9486
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-10-03 13:52:39 -0400

    hardcoded html and css complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Things I Like</h1>
+          <h2>A Photo Gallery by Benjamin Kent Jehl</h2>
         </header>
+        <main>
+          <article className="category">
+            <h3>Panda Bears</h3>
+            <p>
+              Pandas are bears native to south-central China, and are
+              objectively the cutest animals on Earth.
+            </p>
+            <img src="https://cdn-images-1.medium.com/max/1600/1*i1vVm3EqqDIkyucD0079wg.jpeg" />
+          </article>
+          <article className="category">
+            <h3>Miniature Painting</h3>
+            <p>
+              I enjoy painting miniatures. I've only been painting for about
+              6-months , here's some of my work
+            </p>
+            <img src="https://instagram.ftpf1-1.fna.fbcdn.net/vp/ac13de8676b9e7d7b3a4f1aaf38a9a55/5C3728A2/t51.2885-15/e35/32203464_178578342778306_8009127367152762880_n.jpg" />
+          </article>
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
