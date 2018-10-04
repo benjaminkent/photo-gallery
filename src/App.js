b6cf4d54082daf13324f71367bfd434f082eb9d7
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-03 20:42:09 -0400

    html and css done for photo list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import Json from './Json.json'
 import CategoryList from './CategoryList'
 
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
@@ -21,8 +22,24 @@ class App extends Component {
                   <Link to="/">Home</Link>
                 </li>
                 <li>/</li>
-                <li>Panda Bears</li>
+                <li>{Json.pandas.title}</li>
               </ul>
+              <main className="photo-list-main">
+                <div className="photo-list-info">
+                  <h1>{Json.pandas.title}</h1>
+                  <p>{Json.pandas.description}</p>
+                </div>
+                <div className="photo-list">
+                  <div className="photo">
+                    <img src={Json.pandas.photos[0].imageURL} />
+                    <span>{Json.pandas.photos[0].title}</span>
+                  </div>
+                  <div className="photo">
+                    <img src={Json.pandas.photos[1].imageURL} />
+                    <span>{Json.pandas.photos[1].title}</span>
+                  </div>
+                </div>
+              </main>
             </div>
           </div>
         </Router>
