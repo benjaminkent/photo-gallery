e7f19c4a50fe570612a69e0db51140313abed485
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-10-04 10:48:19 -0400

    updated h1 color and added home to homepage

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import CategoryList from './CategoryList'
 
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+
 class Home extends Component {
   render() {
     return (
@@ -9,6 +11,13 @@ class Home extends Component {
           <h1>Things I Like</h1>
           <h2>A Photo Gallery by Benjamin Kent Jehl</h2>
         </header>
+        <ul className="photo-list-header">
+          <li>
+            <Link to="/" component={Home}>
+              Home
+            </Link>
+          </li>
+        </ul>
         <CategoryList />
       </div>
     )
